d13d71213fb2bfaacaa35a80f32dd8a7ad4f525d
Ryna Truong <rynatruong@outlook.com>
2020-08-27 16:37:24 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -61,7 +61,7 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  __
+  Movies.where release_date < 2000 DESC
 end
 
 def can_be_found_updated_and_saved
